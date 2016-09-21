46c8e01e09161d6214a56a2fa5f7a50cf3393c28
strum07 <sagar.strum@gmail.com>
2016-09-21 10:41:34 -0500

    yet another layout change Initialized compute button

--- app/src/main/java/edu/niu/z1783546/baseballstatsapp/MainActivity.java
+++ app/src/main/java/edu/niu/z1783546/baseballstatsapp/MainActivity.java
@@ -2,6 +2,7 @@ package edu.niu.z1783546.baseballstatsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button gobutton;
+
+        gobutton = (Button)findViewById(R.id.button_compute);
+
+        gobutton.setOnClickListener(this);
     }
 }
