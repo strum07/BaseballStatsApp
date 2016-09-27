d395a31209e24b70d3ec1be7ab2f01a13bbb42f2
strum07 <sagar.strum@gmail.com>
2016-09-26 19:55:20 -0500

    renamed collisions

---
+++ app/src/main/java/Baseball.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Isabelle on 9/26/2016.
+ */
+public class Baseball {
+
+}
--- app/src/main/java/edu/niu/z1783546/baseballstatsapp/MainActivity.java
+++ app/src/main/java/edu/niu/z1783546/baseballstatsapp/MainActivity.java
@@ -2,6 +2,7 @@ package edu.niu.z1783546.baseballstatsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
 
         gobutton = (Button)findViewById(R.id.button_compute);
 
-        gobutton.setOnClickListener(this);
+        gobutton.setOnClickListener(compute);
+
+        final View.OnClickListener compute = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        };
     }
 }
